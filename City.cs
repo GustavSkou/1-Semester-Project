999501ae3eae070fa47e99e0e6e4336474bec0ab
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-01 15:35:18 +0100

    city description

--- City.cs
+++ City.cs
@@ -3,6 +3,6 @@ class City : Space
     public City(string name) : base(name)
     {
         paths = ["Horisonten", "VandHullet", "Stien", "Træerne"];
-        spaceDestription = "someDestription";
+        spaceDestription = "Urban populations consume vast amounts of food, water, and energy. \nTo meet these needs, forests are often cleared to make way for agricultural land, \nwhich can lead to habitat loss and biodiversity decline.";
     }
 }
