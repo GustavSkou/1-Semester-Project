ea430349b0f58c8ce368c61892e2528aeb0d2434
dichi24 <diana.chirica222@gmail.com>
2024-11-03 22:18:39 +0100

    Update Farm.cs

--- Farm.cs
+++ Farm.cs
@@ -3,6 +3,6 @@ class Farm : Space
     public Farm(string name) : base(name)
     {
         paths = ["Horisonten", "VandHullet", "Stien", "Træerne"];
-        spaceDestription = "Cattle ranching for beef production is a major threat to forest ecosystems. \nTo make room for feeding land, vast areas of forest are often cleared, which leads to massive deforestation. \nThis not only removes trees that absorb carbon dioxide but also disrupts the entire forest ecosystem, \nimpacting animals by causing them to lose their natural habitats, food sources, and shelter.";
+        spaceDestription = "someDescription";
     }
 }
