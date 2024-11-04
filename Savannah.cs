1a8e733ce9d18ede484a665af76f6659fd453a36
15yasmin <yasminsalim151@gmail.com>
2024-11-04 13:07:45 +0100

    Add files via upload

--- Savannah.cs
+++ Savannah.cs
@@ -3,6 +3,6 @@ class Savannah : Space
     public Savannah(string name) : base(name)
     {
         paths = ["Horisonten", "VandHullet", "Stien", "Træerne"];
-        spaceDestription = "someDestription";
+        spaceDestription = "Welcome to the savanna! \nLand of endless horizons packed with incredible wildlife, and home to the King of the jungle. \n Unfortuanlty, through out the decades, the number of endagereded species has risen due to mass hunting by poachers. \nIn addition, human induced fires during the dry season make the habit for the animals of the savanna incredely difficult to withstand and live in. Help us save the animals and maintain the savanna for the sake of our planet!";
     }
 }
