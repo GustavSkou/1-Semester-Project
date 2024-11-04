5c921b8805de5a710abc192ef4b3c2140c545fa0
zidaanwahid <zidaanwahid@gmail.com>
2024-11-04 12:49:43 +0100

    Update Farm.cs

--- Farm.cs
+++ Farm.cs
@@ -3,6 +3,6 @@ class Farm : Space
     public Farm(string name) : base(name)
     {
         paths = ["Horisonten", "VandHullet", "Stien", "Træerne"];
-        spaceDestription = "someDescription";
+        spaceDestription = "Intensive farming has damaged the soil's health.\n Repeated pesticide use and growing only one crop type remove important nutrients, making the soil less productive.\n Over time, the soil becomes harder to farm and harms animals and insects that need a healthy environment";
     }
 }
