b35a7900247c9e6df6b43bf886561f1da2ee6e00
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-04 14:31:04 +0100

    go works with multipul parameters

--- CommandGo.cs
+++ CommandGo.cs
@@ -1,5 +1,7 @@
 /* Command for transitioning between spaces */
 
+using System.Reflection.Metadata;
+
 class CommandGo : BaseCommand, ICommand
 {
     public CommandGo()
@@ -9,13 +11,8 @@ class CommandGo : BaseCommand, ICommand
 
     public void Execute(Context context, string command, string[] parameters)
     {
-        if (GuardEq(parameters, 1))
-        {
-            Console.WriteLine("I don't seem to know where that is 🤔");
-            return;
-        }
-
+        String parameter = String.Join(" ", parameters);
         Console.Clear();
-        context.Transition(parameters[0]);
+        context.Transition(parameter);
     }
 }
