68aba7b67d367f082a716cbea7444f7b3503e41a
VicMunks <162450014+VicMunks@users.noreply.github.com>
2024-11-04 12:54:39 +0100

    Add files via upload

---
+++ Beach.cs
@@ -0,0 +1,8 @@
+class Beach : Space
+{
+    public Beach(string name) : base(name)
+    {
+        paths = ["Shallow Waters", "Sea Turtle Nesting Site", "Seagull Nesting Area", "Tide Pools"];
+        spaceDestription = "Plastic pollution is a major threat to coastal ecosystems. \nLitter on the beach disrupts habitats and endangers local wildlife. \nSea turtles struggle to nest among the debris, and marine animals often mistake plastic for food, which can be fatal. \nCleaning up the beach will help restore this habitat, allowing animals to live and thrive in a safer environment.";
+    }
+}
