e7f46fb9836ce7b217c6c96958a80a74bdce7faf
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-11 23:03:29 +0100

    spaceing

--- Forest.cs
+++ Forest.cs
@@ -3,7 +3,7 @@ class Forest : Space
     public Forest(string name) : base(name)
     {
         paths = ["Distant Horizon", "Hidden Water Hole", "Forgotten Path", "The Trees"];
-        spaceDestription = "Deforestation is the large-scale removal and destruction of forests, often to make way for agricultural development, urbanization, and industrial activities. \n Forests, which cover around 31% of the Earth's land area, play a vital role in maintaining the balance of the planet's ecosystems. \n They provide habitats for wildlife, help regulate the climate, purify air and water, and maintain the water cycle.";
+        spaceDestription = "Deforestation is the large-scale removal and destruction of forests, often to make way for agricultural development, urbanization, and industrial activities.\nForests, which cover around 31% of the Earth's land area, play a vital role in maintaining the balance of the planet's ecosystems.\nThey provide habitats for wildlife, help regulate the climate, purify air and water, and maintain the water cycle.";
         
         spaceQuestion = "someQuestion";
         spaceAnswerChoices =
