0272850b12a8e96b87e84f89cf14420f4730be2e
VicMunks <vimun23@student.sdu.dk>
2024-11-12 13:44:56 +0100

    improved print()

--- City.cs
+++ City.cs
@@ -3,7 +3,7 @@ class City : Space
     public City(string name) : base(name)
     {
         paths = ["Highway", "Park", "Bikelane", "Alley"];
-        spaceDestription = "Urbanization leads to significant habitat loss and degradation through direct destruction,\n fragmentation, pollution, and the introduction of invasive species.\n This combination severely impacts ecosystems and threatens the survival of many native species.\n Although cities create green spaces to address this problem,\n these efforts do not adequately tackle the root issue of overconsumption.";
+        spaceDestription = "Urbanization leads to significant habitat loss and degradation through direct destruction, fragmentation, pollution, and the introduction of invasive species. This combination severely impacts ecosystems and threatens the survival of many native species. Although cities create green spaces to address this problem, these efforts do not adequately tackle the root issue of overconsumption.";
             
         spaceQuestion = "someQuestion";
         spaceAnswerChoices =
--- Farm.cs
+++ Farm.cs
@@ -3,7 +3,7 @@ class Farm : Space
     public Farm(string name) : base(name)
     {
         paths = ["Distant Horizon", "Hidden Water Hole", "Forgotten Path", "The Trees"];
-        spaceDestription = "Intensive farming has damaged the soil's health.\nRepeated pesticide use and growing only one crop type remove important nutrients, making the soil less productive.\nOver time, the soil becomes harder to farm and harms animals and insects that need a healthy environment";
+        spaceDestription = "Intensive farming has damaged the soil's health. Repeated pesticide use and growing only one crop type remove important nutrients, making the soil less productive. Over time, the soil becomes harder to farm and harms animals and insects that need a healthy environment";
             
         spaceQuestion = "someQuestion";
         spaceAnswerChoices =
