597f7e676605fc58867435b326f7112a8fc0ad24
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-14 15:26:15 +0100

    B

--- Beach.cs
+++ Beach.cs
@@ -27,7 +27,6 @@ class Beach : Biome
 //"Plastic pollution is a major threat to coastal ecosystems. Litter on the beach disrupts habitats and endangers local wildlife. Sea turtles struggle to nest among the debris, and marine animals often mistake plastic for food, which can be fatal. Cleaning up the beach will help restore this habitat, allowing animals to live and thrive in a safer environment.";
 //infoCard = "Did you know that cigarette butts are the most common plastic pollutant found on beaches worldwide? Although they might seem small and harmless, cigarette filters are made of plastic fibers that break down slowly, polluting the sand and water.";
 
-
 class ShallowWaters : Space 
 {
     public ShallowWaters (string name, Biome biome) : base(name, biome)
