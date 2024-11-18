652635f4eb174fdb2e76be3de647be9a3918322b
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-18 15:40:02 +0100

    .

--- City.cs
+++ City.cs
@@ -1,14 +1,14 @@
-/*class City : Biome
+class City : Biome
 {
     public City(string name, Dictionary<string, Space> spaces) : base(name, spaces)
     {        
-        entrySpace = spaces[0];
+        /*entrySpace = spaces[0];
         exitSpace = spaces[2];    
 
         spaces[0].AddEdge(spaces[1].Name, spaces[1]);
         spaces[1].AddEdge(spaces[2].Name, spaces[2]);
-        spaces[2].AddEdge(spaces[0].Name, spaces[0]);
+        spaces[2].AddEdge(spaces[0].Name, spaces[0]);*/
     }
-}*/
+}
     //paths = ["Highway", "Park", "Bikelane", "Alley"];
     //spaceDestription = "Urbanization leads to significant habitat loss and degradation through direct destruction, fragmentation, pollution, and the introduction of invasive species. This combination severely impacts ecosystems and threatens the survival of many native species. Although cities create green spaces to address this problem, these efforts do not adequately tackle the root issue of overconsumption.";
