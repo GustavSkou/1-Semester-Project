7db5f4cd85d67ac3e78499b1dc462304a4f8637f
15yasmin <yasminsalim151@gmail.com>
2024-11-19 13:57:13 +0100

    summary

--- City.cs
+++ City.cs
@@ -1,14 +1,14 @@
 class City : Biome
 {
-    public City(string name, Dictionary<string, Space> spaces) : base(name, spaces)
-    {        
-        /*entrySpace = spaces[0];
-        exitSpace = spaces[2];    
+ public City(string name, Dictionary<string, Space> spaces) : base(name, spaces)
+    {
+        entrySpace = spaces["Highway"];
+        exitSpace = spaces["Bikelane"];
 
-        spaces[0].AddEdge(spaces[1].Name, spaces[1]);
-        spaces[1].AddEdge(spaces[2].Name, spaces[2]);
-        spaces[2].AddEdge(spaces[0].Name, spaces[0]);*/
+      	spaces["Park"].AddEdge(spaces["Bikelane"].Name,spaces["Bikelane"]);
+        spaces["Bikelane"].AddEdge(spaces["Alley"].Name,spaces["Alley"]);
+        spaces["Alley"].AddEdge(spaces["Highway"].Name,spaces["Highway"]);
+        spaces["Highway"].AddEdge(spaces["Park"].Name,spaces["Park"]);
     }
 }
-    //paths = ["Highway", "Park", "Bikelane", "Alley"];
-    //spaceDestription = "Urbanization leads to significant habitat loss and degradation through direct destruction, fragmentation, pollution, and the introduction of invasive species. This combination severely impacts ecosystems and threatens the survival of many native species. Although cities create green spaces to address this problem, these efforts do not adequately tackle the root issue of overconsumption.";
+
