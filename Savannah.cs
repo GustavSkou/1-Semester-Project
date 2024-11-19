e05f53f4b6ce2296b38a8cb55508ee0898711eb0
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-19 15:13:04 +0100

    city

--- Savannah.cs
+++ Savannah.cs
@@ -7,7 +7,7 @@ class Savannah : Biome
         
         spaces["The WaterHole"].AddEdge(spaces["Trees"].Name, spaces["Trees"]);
         spaces["Trees"].AddEdge(spaces["Sunset Ridge"].Name, spaces["Sunset Ridge"]);
-        spaces["Sunset Ridge"].AddEdge(spaces["Thw WaterHole"].Name, spaces["The WaterHole"]);
+        spaces["Sunset Ridge"].AddEdge(spaces["The WaterHole"].Name, spaces["The WaterHole"]);
     }
 	//"Land of endless horizons packed with incredible wildlife, and home to the King of the jungle.\nUnfortuanlty, through out the decades, the number of endagereded species has risen due to mass hunting by poachers.\nIn addition, human induced fires during the dry season make the habit for the animals of the savanna incredely difficult to withstand and live in. Help us save the animals and maintain the savanna for the sake of our planet!";
 }
