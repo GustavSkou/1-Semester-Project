6ae8ccabf7d1fc81d1f302c5dae833cd0d00eec6
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2024-11-26 15:43:48 +0100

    format

--- GameTests/UnitTest1.cs
+++ GameTests/UnitTest1.cs
@@ -1,28 +1,26 @@
-    class Tests
+class Tests
+{
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
+    }
+
+
+    [Test]
+    public void requirementOne()
+    {
+        //There has to be  5 biomes (City, Farm, Savannah, Forest, Beach)
+        World world = new World();
+        Biome[] bimoes = [new City("City", null), new Farm("Farm", null), new Savannah("Savannah", null), new Forest("Forest", null), new Beach("Beach", null)];
+
+        foreach (Biome biome in bimoes)
         {
+            if (world.BiomesSet[biome.Name].GetType() != biome.GetType()) Assert.Fail();
         }
 
+        Assert.Pass();
+    }
 
-        [Test]
-        public void requirementOne()
-        {
-            //There has to be  5 biomes (City, Farm, Savannah, Forest, Beach)
-            World world = new World();
-            Biome[] bimoes = [new City ("City", null), new Farm("Farm", null), new Savannah("Savannah", null), new Forest("Forest", null), new Beach("Beach", null)];
-
-            foreach(Biome biome in bimoes)
-            {
-                if (!world.BiomesSet.ContainsKey(biome.Name))
-                {        
-                    Assert.Fail();
-                }
-            }
-            Assert.Pass();
-        }
-        
     [Test]
     public void requirementfour()
     {
@@ -39,65 +37,82 @@
             }
         }
         Assert.Pass();
-        }
+    }
 
-        [Test]
-        public void requirementTwoRooms()
+    [Test]
+    public void requirementTwoRooms()
+    {
+        //Each biome has 4 rooms 
+
+        World world = new World();
+        Dictionary<string, Space> spacesDict = world.LoadSpaces();
+
+        foreach (Biome bio in world.BiomesSet.Values)
         {
-            //Each biome has 4 rooms 
 
-          World world = new World();
-          Dictionary<string, Space> spacesDict = world.LoadSpaces();
-        
-           foreach(Biome bio in world.BiomesSet.Values)
-          {
+            if (bio.Spaces.Count < 3)
+            {
+                Assert.Fail();
 
-             if (bio.Spaces.Count < 3)
-          {
-              Assert.Fail();
-            
-          }
-            else 
-          {
-            Assert.Pass();
-          }
-          }
+            }
+            else
+            {
+                Assert.Pass();
+            }
         }
-    
+    }
+
     [Test]
     public void requirementTwoQuest()
     {
         //you are asked a question in each room.
         World world = new World();
-     
-     foreach(Biome biome in world.BiomesSet.Values)
-     {
-        foreach(Space space in biome.Spaces.Values)
-        {
 
-            if(space.Quest == null)
+        foreach (Biome biome in world.BiomesSet.Values)
+        {
+            foreach (Space space in biome.Spaces.Values)
             {
-                Assert.Fail();
+
+                if (space.Quest == null)
+                {
+                    Assert.Fail();
+                }
             }
         }
-     }
-    Assert.Pass();
+        Assert.Pass();
+
+        [Test]
+        public void requirementEight()
+        {
+            //The game is done when all spaceses are completed
+
+            foreach (Biome biome in Game.world.BiomesSet.Values)
+            {
+                foreach (Space space in biome.Spaces.Values)
+                {
+                    space.Complete = true;
+                }
+            }
+
+            if (Game.Main([]) == 0) Assert.Pass();
+            else Assert.Fail();
+        }
     }
 
     [Test]
     public void requirementFive()
     {
-        World world = new World(); 
+        World world = new World();
         Dictionary<string, Space> spacesDict = world.LoadSpaces(); //Vil gerne have alle spaces.
 
-        foreach(Space space in spacesDict.Values) //forreach loop. Kigger på hvert space i space dictionaryet.
+        foreach (Space space in spacesDict.Values) //forreach loop. Kigger på hvert space i space dictionaryet.
         {
-            if(space.Quest == null) //"space.Value" går ind i space objectet i the current dictionary entry. ".Quest" checker om der er en quest i det space.
+            if (space.Quest == null) //"space.Value" går ind i space objectet i the current dictionary entry. ".Quest" checker om der er en quest i det space.
             {
                 Assert.Fail($"Room {space.Name} has no quest"); //Efter at have gået ind og tjekket om der er en quest på linje 45, skriver den dette, hvis der ikke er en quest.
             };
         }
         Assert.Pass();
     }
-    
+
 }
