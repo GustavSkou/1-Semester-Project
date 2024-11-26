9db8df229d0dec8d09162726c56507064d9cfa12
15yasmin <yasminsalim151@gmail.com>
2024-11-26 12:58:12 +0100

    Update Game.cs
    afslutning

--- Game/Game.cs
+++ Game/Game.cs
@@ -34,7 +34,7 @@ class Game
             string? line = Console.ReadLine();
             if (line != null) registry.Dispatch(line);
         }
-        Console.WriteLine("Thanks for playing :-)");
+        Console.WriteLine("Congrats! You made it through the jungle of eco quests and have now reached with end.\nAlthough not all questions had one specific solution, your decision making helped solve issues regarding life on land.\nThank you for playing.");
     }
 
     private static void IntroQuestion(Context context)
